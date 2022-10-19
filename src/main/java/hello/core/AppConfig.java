7bf1c76e2d32817e9fdcd6bbb63738905f9f4401
JangYeonji <yunji0220@naver.com>
2022-10-19 21:58:23 +0900

    객체 지향 원리 제공
    새로운 구조와 할인 정책 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -27,7 +28,9 @@ public class AppConfig {
         return new OrderServiceImpl(getMemberRepository(), discountPolicy());
     }
 
+    //할인 정책은 이 부분만 변경해주면 됨
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
