5738e6941ade4233bb3f8582c21a628b855567b9
JangYeonji <yunji0220@naver.com>
2022-10-27 21:38:31 +0900

    싱글톤 컨테이너
    @Configuration과 바이트코드 조작의 마법

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -27,4 +27,25 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    /*
+    * AnnotationConfigApplicationContext에 파라미터로 넘긴 값은 스프링 빈으로 등록된다. 그래서 AppConfig도 스프링 빈이 된다
+    * AppConfig 스프링 빈을 조회해서 클래스 정보를 출력 -> class hello.core.AppConfig$$xxxCGLIB
+    * 이것은 내가 만든 클래스가 아니라 스프링이 CGLIB라는 바이트코드 조작 라이브러리를 사용해서 AppConfig 클래스를 상속받은 임의의 다른 클래스를 만들고,
+    * 그 다른 클래스를 스프링 빈으로 등록한 것이다
+    * 그 임의의 다른 클래스가 바로 싱글톤이 보장되도록 해준다. 아마도 다음과 같이 바이트 코드를 조작해서 작성되어 있을 것이다.
+    * 실제로는 CGLIB의 내부 기술을 사용하는데 매우 복잡하다.
+    * @Bean이 붙은 메서드마다 이미 스프링 빈이 존재하면 존재하는 빈을 반환하고, 스프링 빈이 없으면 생성해서 스프링 빈으로 등록하고 반환하는 코드가 동적으로 만들어진다.
+    * 덕분에 싱글톤이 보장되는 것이다.
+    * AppConfig@CGLIB는 AppConfig 자식 타입으로 AppConfig 타입으로 조회할 수 있다.
+    * AppConfig에 @Configuration 없이 실행하면 순수한 class hello.core.AppConfig가 실행됨
+    * 단, memberrepository가 3번 호출됨 -> 싱글톤 깨짐
+    * */
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass());
+
+    }
 }
