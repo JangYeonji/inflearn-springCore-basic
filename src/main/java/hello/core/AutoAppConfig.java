2fa488129789c5b83717c83f23985184a64a61d2
JangYeonji <yunji0220@naver.com>
2022-10-28 21:12:51 +0900

    컴포넌트 스캔
    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //탐색할 패키지의 시작 위치를 지정. 이 패키지를 포함해서 하위 패키지를 모두 탐색
+        //지정하지 않으면 @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작 위치가 됨 -> 권장
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         //@Configuration도 @ComponentScan이 설정되어 있기 때문에 제외처리. 실무에서는 딱히 제외하진 않음
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication	//Spring Boot는 @ComponentScan이 포함되어 있음->따로 @ComponentScan 없어도 됨
 public class CoreApplication {
 
 	public static void main(String[] args) {
